fa801ad2befebe8f8e9110ee11054b98ea525789
steadham <steadham.scott@gmail.com>
2018-09-10 01:04:00 -0600

    fixing piping

--- fm_tuner.py
+++ fm_tuner.py
@@ -13,7 +13,7 @@ print('Tuning to:', freq)
 p1 = Popen(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000'], stdout=PIPE)
 p2 = Popen('aplay -r 48000 -f S16_LE', stdin=p1.stdout, stdout=PIPE, shell=True)
 (output, err) = p2.communicate()
-print(output, err)
+print('output:', output, 'err:', err)
 tuned_to = re.search('Tuned to .+ Hz\.', output).group(0)
 freq = int(re.search('\d+', tuned_to).group(0)) / 1000000 - .3
 print('Tuned to: ', str(freq))
