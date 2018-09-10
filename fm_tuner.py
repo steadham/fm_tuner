368beaa038b2caeeab9196b774dda9676ffbdeaf
steadham <steadham.scott@gmail.com>
2018-09-09 22:32:26 -0600

    added squelch level

--- fm_tuner.py
+++ fm_tuner.py
@@ -9,6 +9,6 @@ if len(sys.argv) != 2:
 print('Argument List:', str(sys.argv))
 freq = sys.argv[1]
 print('Tuning to:', freq)
-call(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000', '-f 20' '|', 'aplay', '-r 48000', '-f S16_LE'])
+call(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000', '-l 20' '|', 'aplay', '-r 48000', '-f S16_LE'])
 print('called')
 exit(0)
