19bb42cf6aa5e63e90015d344abeb6d984e1070c
steadham <steadham.scott@gmail.com>
2018-09-09 22:57:22 -0600

    added piping

--- fm_tuner.py
+++ fm_tuner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import sys
-from subprocess import PIPE, Popen
+import subprocess
 
 print('Number of arguments:', len(sys.argv))
 if len(sys.argv) != 2:
@@ -9,8 +9,8 @@ if len(sys.argv) != 2:
 print('Argument List:', str(sys.argv))
 freq = sys.argv[1]
 print('Tuning to:', freq)
-p1 = Popen(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000', '-l 20'], stdout=PIPE)
-p2 = Popen(['aplay', '-r 48000', '-f S16_LE'], stdin=p1.stdout, stdout=PIPE)
+p1 = subprocess.Popen(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000', '-l 20'], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(['aplay', '-r 48000', '-f S16_LE'], stdin=p1.stdout)
 output = p2.communicate()[0]
 print(output)
 exit(0)
