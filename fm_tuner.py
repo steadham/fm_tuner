d002c1a44edb97503add07c8b7319b2172dd24a4
steadham <steadham.scott@gmail.com>
2018-09-10 01:00:44 -0600

    fixing piping

--- fm_tuner.py
+++ fm_tuner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import re
 from subprocess import Popen, PIPE
 
 print('Number of arguments:', len(sys.argv))
@@ -10,9 +11,10 @@ print('Argument List:', str(sys.argv))
 freq = sys.argv[1]
 print('Tuning to:', freq)
 p1 = Popen(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000'], stdout=PIPE)
-print('p1 constructed')
 p2 = Popen('aplay -r 48000 -f S16_LE', stdin=p1.stdout, shell=True)
-print('p2 constructed')
 (output, err) = p2.communicate()
 print(output, err)
+tuned_to = re.search('Tuned to .+ Hz\.', output).group(0)
+freq = int(re.search('\d+', tuned_to).group(0)) / 1000000 - .3
+print('Tuned to: ', str(freq))
 exit(0)
