cf7013814e43e5f10ec1e975cb82f6e356b52a2b
steadham <steadham.scott@gmail.com>
2018-09-10 01:14:38 -0600

    try fork

--- fm_tuner.py
+++ fm_tuner.py
@@ -11,9 +11,9 @@ if len(sys.argv) != 2:
 print('Argument List:', str(sys.argv))
 freq = sys.argv[1]
 print('Tuning to:', freq)
-p1 = Popen(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000'], stdout=PIPE, preexec_fn=os.setpgrp)
-p2 = Popen('aplay -r 48000 -f S16_LE', stdin=p1.stdout, stdout=PIPE, shell=True)
-(output, err) = p2.communicate()
+p1 = Popen(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000'], stdout=PIPE, stderr=PIPE, preexec_fn=os.setpgrp)
+p2 = Popen('aplay -r 48000 -f S16_LE', stdin=p1.stdout, stdout=PIPE, stderr=PIPE, shell=True)
+output, err = p2.communicate()
 print('output:', output, 'err:', err)
 tuned_to = re.search('Tuned to .+ Hz\.', output).group(0)
 freq = int(re.search('\d+', tuned_to).group(0)) / 1000000 - .3
