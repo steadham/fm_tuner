0aa44cbda714ce4923b7055c821d4675d4c9d555
steadham <steadham.scott@gmail.com>
2018-09-09 22:35:42 -0600

    added pipe

--- fm_tuner.py
+++ fm_tuner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import sys
-from subprocess import call
+from subprocess import call, PIPE
 
 print('Number of arguments:', len(sys.argv))
 if len(sys.argv) != 2:
@@ -9,6 +9,6 @@ if len(sys.argv) != 2:
 print('Argument List:', str(sys.argv))
 freq = sys.argv[1]
 print('Tuning to:', freq)
-call(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000', '-l 20' '|', 'aplay', '-r 48000', '-f S16_LE'])
+call(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000', '-l 20', PIPE, 'aplay', '-r 48000', '-f S16_LE'])
 print('called')
 exit(0)
