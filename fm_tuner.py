2d833d84b045485ed65776e8d07cd420520626e5
steadham <steadham.scott@gmail.com>
2018-09-09 22:31:11 -0600

    added squelch level

--- fm_tuner.py
+++ fm_tuner.py
@@ -3,11 +3,12 @@
 import sys
 from subprocess import call
 
-print( 'Number of arguments:', len(sys.argv))
+print('Number of arguments:', len(sys.argv))
 if len(sys.argv) != 2:
     exit(1)
 print('Argument List:', str(sys.argv))
 freq = sys.argv[1]
 print('Tuning to:', freq)
-call(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000', '|', 'aplay', '-r 48000', '-f S16_LE'])
+call(['rtl_fm', '-f ' + str(freq) + 'e6', '-s 200k', '-r 48000', '-f 20' '|', 'aplay', '-r 48000', '-f S16_LE'])
+print('called')
 exit(0)
